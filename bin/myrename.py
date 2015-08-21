974efd9f8293e1110e4cf446a0813d0be4391412
jamesp9 <james@jp-it.net.au>
2015-08-21 20:59:42 +1000

    bin: myrename handle dirs properly

--- bin/myrename.py
+++ bin/myrename.py
@@ -15,19 +15,33 @@ if not os.path.exists(src_filename):
     print("{} does't exist".format(src_filename))
     sys.exit()
 
-dst_filename, dst_extension = os.path.splitext(src_filename)
+if os.path.isfile(src_filename):
+    dst_filename, dst_extension = os.path.splitext(src_filename)
+elif os.path.isdir(src_filename):
+    dst_filename, dst_extension = src_filename, ''
 
-chars_to_remove = [' ', ',', '(', ')', '[', ']', "'"]
+chars_to_remove = [' ', ',', '(', ')', '[', ']', "'", '!']
 dst_filename = dst_filename.replace('&', 'and')
 
 for character in chars_to_remove:
     if character in dst_filename:
         dst_filename = dst_filename.replace(character, '.')
 
+# replace with '-' (hyphen)
+annoying_strings = ['.-.', '.-', '.-', '._.']
+for string in annoying_strings:
+    if string in dst_filename:
+        dst_filename = dst_filename.replace(string, '-')
+
+# replace with '_' (underscore)
+annoying_strings = ['_.', '._']
+for string in annoying_strings:
+    if string in dst_filename:
+        dst_filename = dst_filename.replace(string, '_')
+
 dst_filename = dst_filename.replace('..', '.')
-dst_filename = dst_filename.replace('.-.', '-')
-dst_filename = dst_filename.replace('._.', '-')
-dst_filename = re.sub('\.$', '', dst_filename)
+dst_filename = re.sub(r'\.$', '', dst_filename)
+dst_filename = re.sub(r'-$', '', dst_filename)
 
 print("Moving {} => {}".format(src_filename, dst_filename + dst_extension))
 shutil.move(src_filename, dst_filename + dst_extension)
