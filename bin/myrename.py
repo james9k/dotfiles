3bc30a41284eb1f940cbb5c14d7ac606dc6d1adc
jamesp9 <james@jp-it.net.au>
2015-11-06 21:49:37 +1100

    update myrename

--- bin/myrename.py
+++ bin/myrename.py
@@ -1,47 +1,61 @@
 #!/usr/bin/env python3
 
+import glob
+import os
+import re
 import sys
 import shutil
-import re
-import os
 
-if len(sys.argv) > 1:
-    src_filename = sys.argv[1]
-else:
-    print("Please supply filename")
-    sys.exit()
 
-if not os.path.exists(src_filename):
-    print("{} does't exist".format(src_filename))
-    sys.exit()
+def myrename(src_filename):
+    if os.path.isfile(src_filename):
+        filename, extension = os.path.splitext(src_filename)
+    elif os.path.isdir(src_filename):
+        filename, extension = src_filename, ''
 
-if os.path.isfile(src_filename):
-    dst_filename, dst_extension = os.path.splitext(src_filename)
-elif os.path.isdir(src_filename):
-    dst_filename, dst_extension = src_filename, ''
+    chars_to_remove = [' ', ',', '(', ')', '[', ']', "'", '!']
+    filename = filename.replace('&', 'and')
 
-chars_to_remove = [' ', ',', '(', ')', '[', ']', "'", '!']
-dst_filename = dst_filename.replace('&', 'and')
+    for character in chars_to_remove:
+        if character in filename:
+            filename = filename.replace(character, '.')
 
-for character in chars_to_remove:
-    if character in dst_filename:
-        dst_filename = dst_filename.replace(character, '.')
+    # replace with '-' (hyphen)
+    annoying_strings = ['.-.', '.-', '.-', '._.']
+    for string in annoying_strings:
+        if string in filename:
+            filename = filename.replace(string, '-')
 
-# replace with '-' (hyphen)
-annoying_strings = ['.-.', '.-', '.-', '._.']
-for string in annoying_strings:
-    if string in dst_filename:
-        dst_filename = dst_filename.replace(string, '-')
+    # replace with '_' (underscore)
+    annoying_strings = ['_.', '._']
+    for string in annoying_strings:
+        if string in filename:
+            filename = filename.replace(string, '_')
 
-# replace with '_' (underscore)
-annoying_strings = ['_.', '._']
-for string in annoying_strings:
-    if string in dst_filename:
-        dst_filename = dst_filename.replace(string, '_')
+    # filename = filename.replace('..', '.')
+    filename = re.sub('\.+', '.', filename)
+    filename = re.sub(r'\.$', '', filename)
+    filename = re.sub(r'-$', '', filename)
 
-dst_filename = dst_filename.replace('..', '.')
-dst_filename = re.sub(r'\.$', '', dst_filename)
-dst_filename = re.sub(r'-$', '', dst_filename)
+    return filename + extension
 
-print("Moving {} => {}".format(src_filename, dst_filename + dst_extension))
-shutil.move(src_filename, dst_filename + dst_extension)
+
+def mymove(src_filename, dest_filename):
+    print("Moving {} => {}".format(src_filename, dest_filename))
+    shutil.move(src_filename, dest_filename)
+
+
+if __name__ == '__main__':
+    filename_list = []
+
+    if len(sys.argv) > 1:
+        filename_list = sys.argv[1:]
+    else:  # presume all files in cwd
+        filename_list = glob.glob('*')
+
+    for filename in filename_list:
+        if os.path.exists(filename):
+            new_filename = myrename(filename)
+            mymove(filename, new_filename)
+        else:
+            print("{} does't exist".format(filename))
